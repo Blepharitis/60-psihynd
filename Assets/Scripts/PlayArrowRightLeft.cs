c295adc3292e386f92407d9f1dab400964fd216e
Blepharitis <mr.vladislav99999999@gmail.com>
2023-10-14 09:45:25 +0300

    Play Sound Arrow

---
+++ Assets/Scripts/PlayArrowRightLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class PlayArrowRightLeft : MonoBehaviour
+{
+    [SerializeField]
+    [EventRef]
+    private string soundEvent = null; //Обьявляем чтобы менять
+
+    public void PlaySound()  
+    {
+        if(soundEvent != null)
+        {
+            RuntimeManager.PlayOneShot(soundEvent);
+        }
+    }
+}
